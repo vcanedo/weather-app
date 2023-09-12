9c171803aecc6a68ee03fae8bd59bad5cfafbd0f
victor canedo <victorcanedo1@hotmail.com>
2023-09-11 21:36:51 -0300

    index.js adjustment + added fadeIn class in css

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,21 @@
-import { apiKey } from './config.js';
+import { apiKey } from "./config";
 
 document.addEventListener('DOMContentLoaded', function () {
-  let API_KEY = apiKey
+  let API_KEY = apiKey;
 
   const searchButton = document.getElementById('search');
+  const weatherInfo = document.getElementsByClassName('weather-info');
   const locationInput = document.getElementById('location');
   const locationName = document.getElementById('location-name');
   const temperature = document.getElementById('temperature');
   const description = document.getElementById('description');
 
+
   searchButton.addEventListener('click', function () {
+    if (locationInput === '') {
+      return;
+    }
+
       const location = locationInput.value;
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
 
@@ -19,9 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
               locationName.textContent = data.name;
               temperature.textContent = `Temperature: ${data.main.temp}°C`;
               description.textContent = `Description: ${data.weather[0].description}`;
+              weatherInfo.classList.add('fadeIn');
           })
           .catch(error => {
-              console.error('Error fetching weather data:', error);
+              console.error('There was a problem fetching the data:', error);
               locationName.textContent = 'Location not found';
               temperature.textContent = '';
               description.textContent = '';
